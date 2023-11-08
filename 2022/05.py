dc1b1237708191237803c73a6801b26af2808bfb
Lennart Ochel <lennart.ochel@outlook.com>
2023-11-08 19:42:12 +0100

    2022/05

---
+++ 2022/05.py
@@ -0,0 +1,65 @@
+#!../.env/bin/python3
+
+LINES = [line.rstrip() for line in open('05.in')]
+for line in LINES[:3]:
+  print(line)
+
+N = len(LINES)
+print(f'input contains {N} lines')
+
+EMPTY = [i for i,line in enumerate(LINES) if not line]
+print(f'empty lines: {EMPTY}')
+
+
+# 1.
+# --------------------------------------
+def problem1():
+  stacks = []
+  for stack in range(9):
+    stacks.append([LINES[h][stack*4+1] for h in range(EMPTY[0]-2, -1, -1) if LINES[h][stack*4+1] != ' '])
+
+  for line in LINES[EMPTY[0]+1:]:
+    line = line.split()
+    a, b, c = int(line[1]), int(line[3]), int(line[5])
+
+    for x in range(0, a):
+      stacks[c-1].append(stacks[b-1].pop())
+
+  answer = ''
+  for l in stacks:
+    answer += l[-1]
+  return answer
+
+
+# 2.
+# --------------------------------------
+def problem2():
+  stacks = []
+  for stack in range(9):
+    stacks.append([LINES[h][stack*4+1] for h in range(EMPTY[0]-2, -1, -1) if LINES[h][stack*4+1] != ' '])
+
+  for line in LINES[EMPTY[0]+1:]:
+    line = line.split()
+    a, b, c = int(line[1]), int(line[3]), int(line[5])
+
+    tmp = []
+    assert len(stacks[b-1]) >= a, f'{str(stacks[b-1])}, {line}'
+    for x in range(0, a):
+      tmp.append(stacks[b-1].pop())
+    tmp.reverse()
+    for x in tmp:
+      stacks[c-1].append(x)
+
+  answer = ''
+  for l in stacks:
+    answer += l[-1]
+  return answer
+
+
+# --------------------------------------
+if __name__ == '__main__':
+  answer1 = problem1()
+  print(f'Answer 1: {answer1}')
+
+  answer2 = problem2()
+  print(f'Answer 2: {answer2}')
