dd0f5c79c742a965f6768d02ead50808ed77d6e0
Lennart Ochel <lennart.ochel@outlook.com>
2023-11-08 19:48:29 +0100

    2022/06

---
+++ 2022/06.py
@@ -0,0 +1,42 @@
+#!../.env/bin/python3
+
+import itertools
+import math
+import os.path
+import sys
+from collections import defaultdict
+
+input_file = sys.argv[1] if len(sys.argv) > 1 else os.path.splitext(sys.argv[0])[0] + '.in'
+LINES = [line.strip() for line in open(input_file)]
+N = len(LINES)
+print(f'input contains {N} lines')
+
+
+# 1.
+# --------------------------------------
+def problem1():
+  c = list(LINES[0])
+  xx = c[:4]
+  for i in range(4, len(c)+1):
+    xx.pop(0)
+    xx.append(c[i])
+    if len(set(xx)) >=4 :
+      return i+1
+
+
+# 2.
+# --------------------------------------
+def problem2():
+  c = list(LINES[0])
+  xx = c[:14]
+  for i in range(14, len(c)+1):
+    xx.pop(0)
+    xx.append(c[i])
+    if len(set(xx)) >=14 :
+      return i+1
+
+
+# --------------------------------------
+if __name__ == '__main__':
+  print(f'Answer 1: {problem1()}')
+  print(f'Answer 2: {problem2()}')
