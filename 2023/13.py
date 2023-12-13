3401b312721dec48b355b6d96cd273c498d91cb4
Lennart Ochel <lennart.ochel@outlook.com>
2023-12-13 20:45:06 +0100

    Update 2023/13

--- 2023/13.py
+++ 2023/13.py
@@ -1,13 +1,8 @@
 #!../.env/bin/python3
 
-from itertools import combinations
-
 import aoc
 
 def mirror(chunk, a, b, smudges):
-  if b < a:
-    return mirror(chunk, b, a, smudges)
-
   if a < 0 or b >= len(chunk):
     return smudges
 
@@ -18,12 +13,10 @@ def mirror(chunk, a, b, smudges):
 
   if smudges > 1:
     return smudges
+
   return mirror(chunk, a-1, b+1, smudges)
 
 def mirror2(chunk, a, b, smudges):
-  if b < a:
-    return mirror2(chunk, b, a, smudges)
-
   if a < 0 or b >= len(chunk[0]):
     return smudges
 
@@ -34,39 +27,36 @@ def mirror2(chunk, a, b, smudges):
 
   if smudges > 1:
     return smudges
+
   return mirror2(chunk, a-1, b+1, smudges)
 
 # 1.
 # ----------------------------------------
 def problem1():
-  H = 0
-  V = 0
+  answer = 0
   for chunk in aoc.CHUNKS:
-    for a,b in combinations(range(len(chunk)), 2):
-      if abs(a-b) == 1 and 0 == mirror(chunk, a, b, 0):
-        V += min(a, b) + 1
+    for a in range(len(chunk)-1):
+      if 0 == mirror(chunk, a, a+1, 0):
+        answer += 100*(a + 1)
 
-    for a,b in combinations(range(len(chunk[0])), 2):
-      if abs(a-b) == 1 and 0 == mirror2(chunk, a, b, 0):
-        H += min(a, b) + 1
-
-  print(f'Answer 1: {H + 100*V}')
+    for a in range(len(chunk[0])-1):
+      if 0 == mirror2(chunk, a, a+1, 0):
+        answer += a + 1
+  print(f'Answer 1: {answer}')
 
 # 2.
 # ----------------------------------------
 def problem2():
-  H = 0
-  V = 0
+  answer = 0
   for chunk in aoc.CHUNKS:
-    for a,b in combinations(range(len(chunk)), 2):
-      if abs(a-b) == 1 and 1 == mirror(chunk, a, b, 0):
-        V += min(a, b) + 1
+    for a in range(len(chunk)-1):
+      if 1 == mirror(chunk, a, a+1, 0):
+        answer += 100*(a + 1)
 
-    for a,b in combinations(range(len(chunk[0])), 2):
-      if abs(a-b) == 1 and 1 == mirror2(chunk, a, b, 0):
-        H += min(a, b) + 1
-
-  print(f'Answer 2: {H + 100*V}')
+    for a in range(len(chunk[0])-1):
+      if 1 == mirror2(chunk, a, a+1, 0):
+        answer += a + 1
+  print(f'Answer 1: {answer}')
 
 # ----------------------------------------
 if __name__ == '__main__':
