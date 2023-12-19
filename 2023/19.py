dc6f98baca866eb97549e1fa43b693d31ad89ee5
Lennart Ochel <lennart.ochel@outlook.com>
2023-12-19 08:19:01 +0100

    Clean up 2023/19

--- 2023/19.py
+++ 2023/19.py
@@ -1,21 +1,27 @@
 #!../.env/bin/python3
 
-import math
-import statistics
-import sys
-from collections import defaultdict, deque
-from itertools import combinations
-
 import aoc
 
-sys.setrecursionlimit(100000)
+RULES = {}
+for line in aoc.CHUNKS[0]:
+  name, rest = line.split('{')
+  rest = rest[:-1]
+  cond = []
+  default = None
+  for c in rest.split(','):
+    if '<' in c:
+      x, r = c.split('<')
+      y, z = r.split(':')
+      cond.append((x, '<', int(y), z))
+    elif '>' in c:
+      x, r = c.split('>')
+      y, z = r.split(':')
+      cond.append((x, '>', int(y), z))
+    else:
+      default = c
+  RULES[name] = (cond, default)
 
-# x: Extremely cool looking
-# m: Musical (it makes a noise when you hit it)
-# a: Aerodynamic
-# s: Shiny
-
-def accepted(rules, x, m, a, s):
+def accepted(x, m, a, s):
   label = 'in'
   values = {'x': x, 'm': m, 'a': a, 's': s}
 
@@ -25,7 +31,7 @@ def accepted(rules, x, m, a, s):
     elif label == 'R':
       return False
 
-    conditions, default = rules[label]
+    conditions, default = RULES[label]
     label = default
     for (x, op, y, z) in conditions:
       if op == '>' and values[x] > y:
@@ -35,14 +41,10 @@ def accepted(rules, x, m, a, s):
         label = z
         break
 
-def get_accepted_ranges(rules, values, label='in'):
+def get_accepted_ranges(values, label='in'):
   x, m, a, s = values
   lb = {'x': x[0], 'm': m[0], 'a': a[0], 's': s[0]}
   ub = {'x': x[1], 'm': m[1], 'a': a[1], 's': s[1]}
-
-  for vv in 'xmas':
-    if ub[vv] < lb[vv]:
-      return []
 
   result = []
   while True:
@@ -52,11 +54,10 @@ def get_accepted_ranges(rules, values, label='in'):
     elif label == 'R':
       return result
 
-    conditions, default = rules[label]
-    old_label = label
+    conditions, default = RULES[label]
+    current_label = label
     label = default
 
-    # lb <= x <= ub      > y
     for (x, op, y, z) in conditions:
       if op == '>' and ub[x] > y:
         new_lb = {'x': lb['x'], 'm': lb['m'], 'a': lb['a'], 's': lb['s']}
@@ -66,9 +67,9 @@ def get_accepted_ranges(rules, values, label='in'):
         new_ub[x] = y
 
         new_values = ((new_lb['x'], new_ub['x']), (new_lb['m'], new_ub['m']), (new_lb['a'], new_ub['a']), (new_lb['s'], new_ub['s']))
-        result = result + get_accepted_ranges(rules, new_values, old_label)
+        result = result + get_accepted_ranges(new_values, current_label)
 
-        old_label = label
+        current_label = label
         label = z
         break
       elif op == '<' and lb[x] < y:
@@ -79,35 +80,15 @@ def get_accepted_ranges(rules, values, label='in'):
         new_lb[x] = y
 
         new_values = ((new_lb['x'], new_ub['x']), (new_lb['m'], new_ub['m']), (new_lb['a'], new_ub['a']), (new_lb['s'], new_ub['s']))
-        result = result + get_accepted_ranges(rules, new_values, old_label)
+        result = result + get_accepted_ranges(new_values, current_label)
 
-        old_label = label
+        current_label = label
         label = z
         break
-
 
 # 1.
 # ----------------------------------------
 def problem1():
-  rules = {}
-  for line in aoc.CHUNKS[0]:
-    name, rest = line.split('{')
-    rest = rest[:-1]
-    cond = []
-    default = None
-    for c in rest.split(','):
-      if '<' in c:
-        x, r = c.split('<')
-        y, z = r.split(':')
-        cond.append((x, '<', int(y), z))
-      elif '>' in c:
-        x, r = c.split('>')
-        y, z = r.split(':')
-        cond.append((x, '>', int(y), z))
-      else:
-        default = c
-    rules[name] = (cond, default)
-
   answer = 0
   for line in aoc.CHUNKS[1]:
     line = line[1:-1]
@@ -123,7 +104,7 @@ def problem1():
       elif name == 's':
         s = int(value)
 
-    if accepted(rules, x, m, a, s):
+    if accepted(x, m, a, s):
       answer += x+m+a+s
 
   print(f'Answer 1: {answer}')
@@ -131,32 +112,13 @@ def problem1():
 # 2.
 # ----------------------------------------
 def problem2():
-  rules = {}
-  for line in aoc.CHUNKS[0]:
-    name, rest = line.split('{')
-    rest = rest[:-1]
-    cond = []
-    default = None
-    for c in rest.split(','):
-      if '<' in c:
-        x, r = c.split('<')
-        y, z = r.split(':')
-        cond.append((x, '<', int(y), z))
-      elif '>' in c:
-        x, r = c.split('>')
-        y, z = r.split(':')
-        cond.append((x, '>', int(y), z))
-      else:
-        default = c
-    rules[name] = (cond, default)
-
   x = (1, 4000)
   m = (1, 4000)
   a = (1, 4000)
   s = (1, 4000)
 
   answer = 0
-  for vx, vm, va, vs in get_accepted_ranges(rules, (x, m, a, s)):
+  for vx, vm, va, vs in get_accepted_ranges((x, m, a, s)):
     answer += (vx[1]-vx[0]+1)*(vm[1]-vm[0]+1)*(va[1]-va[0]+1)*(vs[1]-vs[0]+1)
   print(f'Answer 2: {answer}')
 
