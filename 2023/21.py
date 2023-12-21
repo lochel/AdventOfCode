a8cc7d1a80d7bafda8847df94e64e0bd7c91129e
Lennart Ochel <lennart.ochel@outlook.com>
2023-12-21 11:26:15 +0100

    2023/21 part 2

--- 2023/21.py
+++ 2023/21.py
@@ -1,14 +1,8 @@
 #!../.env/bin/python3
 
-import math
-import statistics
-import sys
-from collections import defaultdict, deque
-from itertools import combinations
-import copy
-import aoc
+from collections import deque
 
-#sys.setrecursionlimit(10000)
+import aoc
 
 # 1.
 # ----------------------------------------
@@ -36,58 +30,67 @@ def problem1():
 # 2.
 # ----------------------------------------
 def problem2():
-  # - In exactly 6 steps, he can still reach 16 garden plots.
-  # - In exactly 10 steps, he can reach any of 50 garden plots.
-  # - In exactly 50 steps, he can reach 1594 garden plots.
-  # - In exactly 100 steps, he can reach 6536 garden plots.
-  # - In exactly 500 steps, he can reach 167004 garden plots.
-  # - In exactly 1000 steps, he can reach 668697 garden plots.
-  # - In exactly 5000 steps, he can reach 16733044 garden plots.
-
   map = aoc.parseLines(aoc.LINES, lambda line : list(line))
 
   for x,y in aoc.Grid(map):
     if map[y][x] == 'S':
       pos = (x, y)
 
-
-
   Q = deque()
-  end = defaultdict(set)
-  end[pos].add((0, 0))
-  for step in range(10):
-    Q = deque(end.items())
-    end.clear()
+  end = set([pos])
+
+  countEven, countOdd = None, None
+  count = {}
+  step = 0
+  while True:
+    step += 1
+    Q = deque(end)
+    end = set()
 
     while Q:
-      (x, y), z = Q.pop()
-      for (xx, yy) in aoc.get_neighbors(x, y, False, False):
-        if map[yy % aoc.N][xx % aoc.M] in '.S':
-          end[(xx, yy)] = z
-    print(step, sum([len(x) for x in end.values()]))
-  answer = sum([len(x) for x in end.values()])
+      x, y = Q.pop()
+      for (xx, yy) in aoc.get_neighbors(x, y, False, True):
+        if map[yy][xx] in '.S':
+          end.add((xx, yy))
+
+    count[step] = len(end)
+
+    if count[step] in [countEven, countOdd] and count[step-1] in [countEven, countOdd]:
+      break
+
+    if step % 2 == 0:
+      countEven = len(end)
+    else:
+      countOdd = len(end)
+
+  n = (26501365 - aoc.N//2) // aoc.N
+
+  cornersEven = countEven - count[64]
+  cornersOdd  = countOdd - count[65]
+
+  answer = (n+1)**2 * countOdd + n**2 * countEven - (n+1) * cornersOdd + n * cornersEven
   print(f'Answer 2: {answer}')
 
+  #       XXX
+  #       XXX
+  #       XXX
+  #    XXX   XXX
+  #    XXX   XXX
+  #    XXX   XXX
+  # XXX         XXX
+  # XXX    S    XXX
+  # XXX         XXX
+  #    XXX   XXX
+  #    XXX   XXX
+  #    XXX   XXX
+  #       XXX
+  #       XXX
+  #       XXX
 
-
-
-
-  end.clear()
-  end[pos].add((0, 0))
-  for step in range(10):
-    Q = deque(end.items())
-    end.clear()
-
-    while Q:
-      (x, y), z = Q.pop()
-      for (xx, yy) in aoc.get_neighbors(x, y, False, False):
-        if map[yy % aoc.N][xx % aoc.M] in '.S':
-          end[(xx % aoc.M, yy % aoc.N)].add((xx//aoc.M, yy//aoc.N))
-          for zz in z:
-            end[(xx % aoc.M, yy % aoc.N)].add(zz)
-    print(step, sum([len(x) for x in end.values()]))
-  answer = sum([len(x) for x in end.values()])
-  print(f'Answer 2: {answer}')
+  # 631357595415612: That's not the right answer; your answer is too low.
+  # 631357543626812: That's not the right answer; your answer is too low.
+  # 631357543626746: That's not the right answer; your answer is too low.
+  # 631357595415546: That's not the right answer; your answer is too low.
 
 # ----------------------------------------
 if __name__ == '__main__':
